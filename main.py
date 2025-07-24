f23076e655b7d49798e183607c6dd2c89c6c30ad
Alpha team <131553786+Alpharobocup@users.noreply.github.com>
2025-07-24 23:25:04 +0330

    Update main.py

--- main.py
+++ main.py
@@ -157,4 +157,6 @@ bot.set_webhook(url=WEBHOOK_URL)
 
 # --- اجرا ---
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
