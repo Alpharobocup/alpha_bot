60288874052727fd346f6f1a3f52acac2e7413c7
Alpha team <131553786+Alpharobocup@users.noreply.github.com>
2025-07-25 22:24:20 +0330

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def menu_handler(message):
     elif text == "📞 ارتباط با مدیریت":
         bot.send_message(chat_id, "در حال ارسال پیام به مدیریت...", reply_markup=main_menu())
         uername = message.from_user.username 
-        }
+        
         # ارسال پیام به ادمین با اطلاعات کاربر و متن آماده
         bot.send_message(ADMIN_ID, f"👤 کاربر  (( { message.from_user.id } || { message.from_user.first_name } ))  درخواست ارتباط داده:\n\nسلام ــ تبادل \n @{uername}")
         bot.send_message(chat_id, "پیام شما به مدیریت ارسال شد. لطفاً منتظر پاسخ باشید.", reply_markup=main_menu())
